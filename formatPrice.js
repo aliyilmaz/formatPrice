4fda7da469bc179df5707af51af03cb4b3417034
Ali Yılmaz <127128093+aliyilmaz@users.noreply.github.com>
2023-11-18 01:39:45 +0300

    Update formatPrice.js

--- formatPrice.js
+++ formatPrice.js
@@ -1,16 +1,22 @@
 function formatPrice(price) {
-    // Convert price to number
+    // Check if the price is a string containing commas, and remove commas if present.
+    if (typeof price === 'string' && price.includes(',')) {
+        price = price.replace(/,/g, '');
+    }
+
+    // Convert the price to a floating-point number.
     price = parseFloat(price);
 
-    // Separate the decimal part and whole part of the price
+    // Extract the decimal and integer parts of the price.
     const decimalPart = (price % 1).toFixed(2);
     const integerPart = Math.floor(price);
 
-    // Break the whole part into its steps
+    // Format the integer part by adding commas for thousands separation.
     const formattedIntegerPart = integerPart.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-    // Create result by combining decimal and whole part
+    // Combine the formatted integer part and the decimal part to create the final formatted price.
     const formattedPrice = formattedIntegerPart + '.' + decimalPart.substring(2);
 
+    // Return the formatted price as a string.
     return formattedPrice;
 }
